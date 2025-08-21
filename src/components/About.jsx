ec0098fb5c515a8e6b03f38afe38353256b438f8
Abash Ansari <ansariabash2004@gmail.com>
2025-08-21 15:35:23 +0530

    added a hr tag

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div id="about">
+      <hr />
       <p>I am <b> Abash Ansari</b> ,A student of MSU prusing for my cloud Computing...<br/>
       I am form Dentam Bazar , W/Skkim<br/>
       My <b>Aim</b> is to be a <u>politician..</u><br/>
