67749872b0c00ff48fd1a2a8fe400d2cd58a82a8
Abash Ansari <ansariabash2004@gmail.com>
2025-08-21 13:36:26 +0530

    added a Anchor tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 
+import About from './About'
 export default function Navbar() {
   return (
     <>
     <nav>
-    <h1>My Portfolio</h1>
-    <hr />
+     <div className='left'>
+    <h3>My Portfolio</h3>
+     </div>
+     <div className='right'>
+      <a href='#about'>About</a>
+      <a herf='#project'>Projects</a>
+      <a href='#contact'>Contact</a>
+     </div>
     </nav>
     </>
   )
