ab95fd47dc23c05bee6418d89b37575bcdf38c3f
Abash Ansari <ansariabash2004@gmail.com>
2025-08-21 13:37:45 +0530

    added a id

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
+    <div id="about">
       <p>I am <b> Abash Ansari</b> ,A student of MSU prusing for my cloud Computing...<br/>
       I am form Dentam Bazar , W/Skkim<br/>
       My <b>Aim</b> is to be a <u>politician..</u><br/>
